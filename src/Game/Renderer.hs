8f1b058872b27fa12f8d4cf6820ffb63c786b3d5
Dragor Draganović <dragor.draganovic@gmail.com>
2022-09-14 22:38:58 +0200

    Heavily enhanced UI rendering
    Implemented table system, which is now used for rendering some UI elements. Basic game description is also added.

--- src/Game/Renderer.hs
+++ src/Game/Renderer.hs
@@ -8,7 +8,7 @@ import System.Console.ANSI
 import System.IO (stdout, hFlush)
 import Text.Printf
 
-import Game.Data.Map ( MapTile, Map, Terrain (..), LandType (..), WaterType (..), Object (..), GameState, Effect (..), MapPos)
+import Game.Data.Map ( MapTile, Map, Terrain (..), LandType (..), WaterType (..), Object (..), GameState, Effect (..), MapPos, mapSize)
 import Game.Config ( fireMxLvl )
 
 
@@ -35,13 +35,90 @@ showf d = printf ("%." ++ show d ++ "f") . (/(10^d)) . itf
 render :: GameState -> IO ()
 render (mp, res, act, cr) = do
     clearTerminal
+
     renderMap mp cr
-    renderText $ "resources: " ++ showf 1 res
-    renderText $ " | actions left: " ++ show act ++ "\n\n"
-    renderText "c - Plant Crops (cost:  5 res., 1 act.)\n"
-    renderText "h - Build House (cost: 50 res., 1 act.)\n\n"
+    renderText "\n"
+
+    let w = fst (mapSize mp)
+    renderResAct res act
+    renderText "\n"
+    renderText $ "Press [Space] key for NEXT TURN" ++ "\n\n"
+    
+    let infotitle = " INFO "
+    renderText $ hline "*" (w - 3) ++ infotitle ++ hline "*" (w - 3) ++ "\n\n"
+    
+    renderText $ "> RESOURCES ($) are used for performing ACTIONS" ++ "\n"
+    renderText $ "  - Each CROP field adds $ to total $ amount every turn" ++ "\n"
+    renderText $ "  - Crop fields $ yield: [;;] 0.1, [ii] 0.2, [ll] 0.3" ++ "\n"
+    renderText "\n"
+    renderText $ "> MAN-POWER (#) is used for performing ACTIONS" ++ "\n"
+    renderText $ "  - Every turn # resets to value that is proportional to number of HOUSES built" ++ "\n"
+    renderText $ "  - Each house provides 5 #" ++ "\n"
+    renderText "\n"
+
+    renderKeyMappings
+    renderText "\n"
+
     --renderText "(everything is placed on the tile where is cursor currently)\n"
 
+
+renderResAct :: Int -> Int -> IO () -- └ ┘ ┼ ─ ┴ ├ ┤ ┬ ┌ ┐ │
+renderResAct res act = do    
+    let restbl = ["Resources", "$", showf 1 res]
+    let acttbl = ["Man-power", "#", show act]
+    renderText $ mkTbl [restbl] (map length restbl)
+    renderText $ mkTbl [acttbl] (map length acttbl)
+
+renderKeyMappings :: IO ()
+renderKeyMappings = do
+    let (cols, lens) = unzip [stdLen ["Key", "H","U","Y"]
+            , stdLen ["Action", "Build House","Plant Crops","Fight Fire"]
+            , stdLen ["Cost in $", "50.0","5.0","5.0"]
+            , stdLen ["Cost in #", "1","1","1"]
+            ]
+    let rows = transpose cols
+
+    let txt = mkTbl rows lens
+    renderText $ txt ++ "\n\n"
+
+-- Standardize length (add spaces so every string is of same length)
+stdLen :: [String] -> ([String], Int)
+stdLen sx = do
+    let lens = map length sx
+    let mxlen = maximum lens
+    let toadd = map ((`replicate` ' ') . (mxlen -)) lens
+    (zipWith (++) sx toadd, mxlen)
+
+hline :: String -> Int -> String
+hline line len = concat (replicate len line)
+
+hTblLine :: String -> String -> String -> String -> [Int] -> String
+hTblLine l r sep line lens = l ++ foldr ((\x y -> x ++ sep ++ y) . hline line) "" (init lens) ++ hline line (last lens) ++ r
+
+tblTopLine :: [Int] -> String
+tblTopLine = hTblLine "┌─" "─┐" "─┬─" "─"
+
+tblBtmLine :: [Int] -> String
+tblBtmLine = hTblLine "└─" "─┘" "─┴─" "─"
+
+tblMidLine :: [Int] -> String
+tblMidLine = hTblLine "├─" "─┤" "─┼─" "─"
+
+mkTblRow :: [String] -> String
+mkTblRow row = "│ " ++ concatMap (++ " │ ") (init row) ++ last row ++ " │"
+
+mkTbl :: [[String]] -> [Int] -> String
+mkTbl [] _ = ""
+mkTbl rows lens = do
+    let txt = foldr1 (\x y -> x ++ "\n" ++ tblMidLine lens ++ "\n" ++ y) (map mkTblRow rows)
+    tblTopLine lens ++ "\n" ++ txt ++ "\n" ++ tblBtmLine lens ++ "\n"
+
+
+transpose:: [[a]]->[[a]]
+transpose ([]:_) = []
+transpose x = map head x : transpose (map tail x)
+
+--mkRow :: [String] -> String
 
 {- Map rendering logic -}
 
@@ -56,8 +133,7 @@ renderMap mp cr = do
 -- Recursive loop to render map (map -> current pos -> cursor pos -> IO)
 renderMapLoop :: Map -> MapPos -> MapPos -> IO ()
 
-renderMapLoop [] _ _ = do                           -- when map is finished rendering
-    putStr ""
+renderMapLoop [] _ _ = putStr ""
 
 renderMapLoop ([]:cls) (_,y) cr = do                -- when one row of map is finished rendering
     putStrLn ""
@@ -136,7 +212,7 @@ strForTile :: MapTile -> String
 -- Crop
 strForTile (_, _, _, Fire x _)
         | x == 1 || x == fireMxLvl       = ",,"
-        | x == 2 || x == (fireMxLvl - 1) = "vv"
+        | x == 2 || x == fireMxLvl - 1 = "vv"
         | otherwise = "MM"
 strForTile (_, Crop x, _, _)
         | x == 0    = ";;"
